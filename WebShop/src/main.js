42d7b9492bca8582b71bedbcc0a193a83dd0267a
Marcello Fidjestøl <mefidjes@stud.ntnu.no>
2023-03-19 19:12:51 +0100

    Added a map component
    Added an api key for google maps
    and components.

--- WebShop/src/main.js
+++ WebShop/src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from "./stores";
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
 
 import './assets/main.css'
 
@@ -12,5 +14,10 @@ const app = createApp(App)
 app.use(createPinia());
 app.use(router);
 app.use(store);
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyCGte24sbY2tFONaLk-lq_fC4ImNQ24opg        ',
+    },
+})
 
 app.mount('#app');
--- WebShop/src/views/HomeView.vue
+++ WebShop/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/ProductPage.vue'
+import Map from '../components/Map.vue';
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Map></Map>
   </main>
 </template>
