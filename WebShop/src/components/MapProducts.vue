545ce579d42d6977adbd8ecf2e244f4d8b504074
Marcello Fidjestøl <mefidjes@stud.ntnu.no>
2023-03-26 15:21:34 +0200

    Fixed the map to accurately display icon

--- WebShop/src/components/MapProducts.vue
+++ WebShop/src/components/MapProducts.vue
@@ -15,7 +15,7 @@
         style="width: 500px; height: 300px"
       >
         <GMapCluster>
-          <GMapMarker :key="index" v-for="(m, index) in info" :position="{lat: m.latitude, lng: m.longitude}" :clickable="true" :draggable="false"
+          <GMapMarker :key="index" v-for="(m, index) in products" :position="{lat: m.latitude, lng: m.longitude}" :clickable="true" :draggable="false"
         @click="openMarker(m.id)" >
           <GMapInfoWindow
           :closeclick="true"
@@ -30,19 +30,29 @@
 </template>
 
 <script>
+import axios from 'axios'
+
+
 export default {
   data() {
     return {
+      products: [],
       center: { lat: 63.4255, lng: 10.3952 },
       options: {
         mapId: '1e8c8a920e03249a'
       },
     }
   },
-  props: {
-    products: {
-      type: Array
-    }
+  mounted() {
+    axios
+      .get('http://localhost:8001/products')
+      .then((response) => {
+        this.products = response.data
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   },
   methods: {
     openMarker(id) {
